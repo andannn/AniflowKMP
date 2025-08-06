d7c4c90e03640f5533dec905c67018276c421b57
andannn <jqn296763005@gmail.com>
2025-08-06 17:25:39 +0900

    remove useless api export

--- androidApp/src/androidMain/kotlin/me/andannn/aniflow/ui/Track.kt
+++ androidApp/src/androidMain/kotlin/me/andannn/aniflow/ui/Track.kt
@@ -4,16 +4,20 @@
  */
 package me.andannn.aniflow.ui
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import com.arkivanov.decompose.extensions.compose.subscribeAsState
 import me.andannn.aniflow.components.track.TrackComponent
 import me.andannn.aniflow.data.model.relation.MediaWithMediaListItem
+import me.andannn.aniflow.ui.widget.MediaRowItem
 
 @Composable
 fun Track(
@@ -27,18 +31,31 @@ fun Track(
     )
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TrackContent(
     content: List<MediaWithMediaListItem>,
     modifier: Modifier = Modifier,
 ) {
-    LazyColumn(modifier) {
-        items(
-            content,
-            key = { it.hashCode() },
-        ) { item ->
-            Text(item.toString())
-            Divider()
+    Scaffold(
+        modifier = modifier,
+        topBar = {
+            TopAppBar(
+                title = { Text("Track") },
+            )
+        },
+    ) {
+        LazyColumn(Modifier.padding(top = it.calculateTopPadding())) {
+            items(
+                items = content,
+                key = { it.mediaListModel.id },
+            ) { item ->
+                val media = item.mediaModel
+                MediaRowItem(
+                    title = media.title?.romaji.toString(),
+                    coverImage = media.coverImage,
+                )
+            }
         }
     }
 }
