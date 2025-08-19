92366956fb7573faa78ee4812c77eeb0350927b7
andannn <jqn296763005@gmail.com>
2025-08-19 20:35:15 +0900

    upgrade compose to 1.9.0

--- androidApp/src/androidMain/kotlin/me/andannn/aniflow/ui/widget/MediaPreviewItem.kt
+++ androidApp/src/androidMain/kotlin/me/andannn/aniflow/ui/widget/MediaPreviewItem.kt
@@ -31,9 +31,9 @@ import androidx.compose.ui.graphics.Color.Companion.White
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil3.compose.AsyncImage
-import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 fun MediaPreviewItem(
